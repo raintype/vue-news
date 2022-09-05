0b54d1c28e6f542b08c8a3aece8bfa97c4055995
raintype <raintype@nexon.co.kr>
2022-09-05 16:04:29 +0900

    'init'

---
+++ src/views/NewsView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-for="user in users">{{ user }}</div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+            .then(function(response) {
+                //this.users = response.data);
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+
+            })
+    },
+}
+</script>ß
+
+<style>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+
+module.exports = {
+  lintOnSave: false
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false
 })
